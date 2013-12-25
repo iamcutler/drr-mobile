29a5977dbe5939334e0bc448ec940d234173126d
Allan Cutler <musicfan03@gmail.com>
2013-12-24 17:11:16 -0800

    Added static voting view.

--- app/scripts/controllers/voting.js
+++ app/scripts/controllers/voting.js
@@ -1,5 +1,8 @@
 'use strict';
 
-app.controller('VotingController', function ($scope) {
-  
+app.controller('VotingController', function ($scope, $state) {
+  // Voting
+  if($state.current.name == "vote") {
+    $scope.title = "Vote for Januarys Dirty Girl!";
+  }
 });
