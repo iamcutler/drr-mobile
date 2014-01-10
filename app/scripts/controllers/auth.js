9502a40f309361a126e83bc30b6eeafc0dc0db2d
Allan Cutler <musicfan03@gmail.com>
2014-01-09 19:28:37 -0800

    Added logout view and logout state in auth controller

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -2,4 +2,9 @@
 
 app.controller('AuthController', function ($scope) {
 
+  // Logout
+  if($state.current.name == "logout") {
+    AuthService.clear_user_session();
+    AuthService.logout();
+  }
 });
