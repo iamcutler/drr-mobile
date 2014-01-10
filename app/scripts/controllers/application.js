522bb5f3849eb580d5e72d2eca3a64cff94947dd
Allan Cutler <musicfan03@gmail.com>
2014-01-09 19:36:40 -0800

    Added CDN domain constant to application scope, checking user access and setting current_user though out the application.

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -1,5 +1,12 @@
 'use strict';
 
-app.controller("ApplicationController", function() {
-  
+app.controller("ApplicationController", function($scope, $state, $cookieStore, cdnDomain, AuthService) {
+  // Set CDN host in app scope
+  $scope.cdnDomain = cdnDomain;
+
+  // Check user access throughout the application and redirect if wrong privileges
+  AuthService.check_user_access();
+
+  // Current user
+  $scope.current_user = AuthService.current_user();
 });
