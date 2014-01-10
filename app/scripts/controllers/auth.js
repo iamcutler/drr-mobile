c23198016dc0b8106662980372da2af93756ded9
Allan Cutler <musicfan03@gmail.com>
2014-01-09 19:30:33 -0800

    Introduced login state and scope function to process logging in

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -1,6 +1,29 @@
 'use strict';
 
 app.controller('AuthController', function ($scope) {
+  // Login
+  if($state.current.name == "login") {
+    $scope.login = {};
+    $scope.loginSubmitted = false;
+    $scope.loginError = '';
+
+    $scope.processLogin = function(form) {
+      // If form is valid, call auth login service
+      if(form.$valid) {
+        AuthService.login($scope.login, function(data) {
+          if(data.status) {
+            // Set user session and redirect to user feed
+            AuthService.set_user_session(data.user);
+            $location.path('/feed');
+          } else {
+            $scope.loginError = 'wrong username and/or password. please try again.';
+          }
+        });
+      } else {
+        $scope.loginSubmitted = true;
+      }
+    }
+  }
 
   // Logout
   if($state.current.name == "logout") {
