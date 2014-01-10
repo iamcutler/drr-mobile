fc44c275711f47a3c72a8746bc72a8ed99fb58d9
Allan Cutler <musicfan03@gmail.com>
2014-01-09 19:32:14 -0800

    Added registration state to processing registrations, check username uniqueness, and form validations

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -1,6 +1,59 @@
 'use strict';
 
 app.controller('AuthController', function ($scope, $state, $location, $cookieStore, AuthService) {
+  // Registration
+  if($state.current.name == "registeration") {
+    $scope.register = {};
+    $scope.registerSubmitted = false;
+    $scope.registerErrors = false;
+    $scope.usernameTaken = false;
+
+    // Watch desired username and call username unique service
+    $scope.check_username_uniqueness = function() {
+      console.log('check username');
+      AuthService.check_username_uniqueness($scope.register.username, function(data) {
+        if(data.unique) {
+          $scope.usernameTaken = false;
+
+          $("input[name='username']").css({
+            background: '#FFF',
+            color: '#555'
+          });
+        } else {
+          $scope.usernameTaken = true;
+
+          $("input[name='username']").css({
+            background: '#F00',
+            color: '#000'
+          });
+        }
+      });
+    }
+
+    // Process registartion
+    $scope.processRegistration = function(form) {
+      $scope.registerSubmitted = true;
+    
+      // If form is valid, call auth register service
+      if(form.$valid) {
+        AuthService.register($scope.register, function(data) {
+          if(data.status) {
+            // Save user data in cookie
+            AuthService.set_user_session(data);
+
+            console.log('Registered user successfully');
+            // Successful redirect to user feed
+            $location.path('/feed');
+          } else {
+            $scope.registerErrors = true;
+            $("div.alert.register-errors").html(data.message);
+            console.log(data);
+          }
+        });
+      }
+    }
+  }
+
   // Login
   if($state.current.name == "login") {
     $scope.login = {};
