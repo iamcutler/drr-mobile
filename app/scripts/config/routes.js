4b03055f459ccfa1e0352dcc8693918e999fcc9c
Allan Cutler <musicfan03@gmail.com>
2014-01-09 19:43:11 -0800

    Introduced an access object to routing to track user privileges.

--- app/scripts/config/routes.js
+++ app/scripts/config/routes.js
@@ -22,68 +22,101 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
       parent: "auth",
       url: "/login",
       controller: "AuthController",
-      templateUrl: "/views/auth/login.html"
+      templateUrl: "/views/auth/login.html",
+      access: {
+        require_user: false,
+        allowLoggedIn: false
+      }
     })
     .state("registeration", {
       parent: "auth",
       url: "/register",
       controller: "AuthController",
-      templateUrl: "/views/auth/register.html"
+      templateUrl: "/views/auth/register.html",
+      access: {
+        require_user: false,
+        allowLoggedIn: false
+      }
+    })
     })
     // New Feed
     .state("feed", {
       parent: "default",
       url: "/feed",
       controller: "FeedController",
-      templateUrl: "/views/news-feed/index.html"
+      templateUrl: "/views/news-feed/index.html",
+      access: {
+        require_user: true
+      }
     })
     // My Messages
     .state("messages", {
       parent: "default",
       url: "/messages",
       controller: "MessagesController",
-      templateUrl: "/views/messages/index.html"
+      templateUrl: "/views/messages/index.html",
+      access: {
+        require_user: true
+      }
     })
     // Friend Requests
     .state("friend-requests", {
       parent: "default",
       url: "/requests",
       controller: "RequestsController",
-      templateUrl: "/views/friend-requests/index.html"
+      templateUrl: "/views/friend-requests/index.html",
+      access: {
+        require_user: true
+      }
     })
     // Events
     .state("search-events", {
       parent: "default",
       url: "/search-events",
       controller: "EventsController",
-      templateUrl: "/views/events/search.html"
+      templateUrl: "/views/events/search.html",
+      access: {
+        require_user: true
+      }
     })
     // Media
     .state("media", {
       parent: "default",
       url: "/media",
       controller: "MediaController",
-      templateUrl: "/views/media/index.html"
+      templateUrl: "/views/media/index.html",
+      access: {
+        require_user: true
+      }
     })
     // Dirty Girls
     .state("dirty-girls", {
       parent: "default",
       url: "/dirty-girls",
       controller: "DirtyGirlsController",
-      templateUrl: "/views/dirty-girls/index.html"
+      templateUrl: "/views/dirty-girls/index.html",
+      access: {
+        require_user: true
+      }
     })
     // Voting
     .state("vote", {
       parent: "default",
       url: "/vote",
       controller: "VotingController",
-      templateUrl: "/views/voting/vote.html"
+      templateUrl: "/views/voting/vote.html",
+      access: {
+        require_user: true
+      }
     })
     // Report bugs/problems
     .state("report-bug", {
       parent: "default",
       url: "/report",
       controller: "ReportController",
-      templateUrl: "/views/report/bug.html"
+      templateUrl: "/views/report/bug.html",
+      access: {
+        require_user: true
+      }
     });
 });
