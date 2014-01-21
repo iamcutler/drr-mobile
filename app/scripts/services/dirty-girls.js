e1f918521a7b1ff19fefd2902f5b0ff12b83dddc
Allan Cutler <musicfan03@gmail.com>
2014-01-20 19:41:05 -0800

    Refactored dirty girls service to return promises.

--- app/scripts/services/dirty-girls.js
+++ app/scripts/services/dirty-girls.js
@@ -1,45 +1,45 @@
 'use strict';
 
-app.factory('DirtyGirlsService', function ($resource, apiPrefix, AuthService) {
-  var DirtyGirl = $resource(apiPrefix + '/dirty-girls/:id', {
-    user_hash: AuthService.current_user().hash
-  });
-
+app.factory('DirtyGirlsService', function ($resource, $q, apiPrefix, AuthService) {
   return {
-    get_dirty_girls: function (callback) {
-      DirtyGirl.query({}, function(data) {
-        if(typeof callback === "function" && data != undefined) {
-          callback(data);
-        } else {
-          console.log('Error fetching dirty girls');
-        }
+    get_dirty_girls: function () {
+      var deferred = $q.defer(),
+          DirtyGirl = $resource(apiPrefix + '/dirty-girls', { user_hash: AuthService.current_user().hash });
+
+      DirtyGirl.query({}, function(response) {
+        deferred.resolve(response);
+      }, function() {
+        deferred.reject();
       });
+
+      return deferred.promise;
     },
-    find_by_id: function(id, callback) {
-      var girl = DirtyGirl.get({id: id}, function() {
-        if(typeof callback === "function" && girl != undefined) {
-          callback(girl);
-        }
+    get_girl_by_id: function() {
+      var deferred = $q.defer(),
+          DirtyGirl = $resource(apiPrefix + '/dirty-girls/:id', { user_hash: AuthService.current_user().hash });
+
+      // Call service to fetch dirty girl
+      DirtyGirl.get({id: 3}, function(response) {
+        deferred.resolve(response);
+      }, function() {
+        deferred.reject();
       });
+
+      return deferred.promise;
     }
+    /*get_current_polling: function(callback) {
+      $http.post(apiPrefix + '/dirty-girls/current_voting', { user_hash: AuthService.current_user().hash }).
         success(function(data) {
           if(typeof callback === "function") {
             callback(data);
           }
         }).
         error(function() {
-          callback({error: true});
+
         });
     },
-    find_by_id: function(params, girls) {
-      var i;
+    cast_vote: function() {
 
-      for(i = 0; i <= Number(girls.length - 1); i++) {
-        // Match id param with dirty girl id
-        if(girls[i].id == params.id) {
-          return girls[i];
-        }
-      }
-    }
+    }*/
   };
 });
