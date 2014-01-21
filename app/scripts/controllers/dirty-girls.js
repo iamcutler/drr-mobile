5759fdb7b2a2119ae6074d7ac15af61dd4dd7fd4
Allan Cutler <musicfan03@gmail.com>
2014-01-20 20:12:27 -0800

    Refactored dirty girls controller after promise modifications

--- app/scripts/controllers/dirty-girls.js
+++ app/scripts/controllers/dirty-girls.js
@@ -1,22 +1,17 @@
 'use strict';
 
-app.controller('DirtyGirlsController', function ($scope, $state, $stateParams, DirtyGirlsService) {
-  // Call to service to fetch data
-  DirtyGirlsService.get_dirty_girls(function(data) {
-    $scope.dirty_girls = data;
-  });
-
+app.controller('DirtyGirlsController', function ($scope, $state, $stateParams, dirty_girls) {
   // Dirty Girls
   if($state.current.name == "dirty-girls") {
     $scope.title = "Dirty Girls";
+    // Call to service to fetch data
+    $scope.dirty_girls = dirty_girls;
   }
 
   // Dirty girl page
   if($state.current.name == "dirty-girl-page") {
     // Call dirty girl service to fetch dirty girl data
-    DirtyGirlsService.find_by_id($stateParams.id, function(data) {
-      $scope.title = data.name;
-      $scope.dirty_girl = data;
-    });
+    $scope.title = dirty_girls.name;
+    $scope.dirty_girl = dirty_girls;
   }
 });
