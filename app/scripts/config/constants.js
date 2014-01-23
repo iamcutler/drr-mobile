c73a92e4acfdc311d99e36bb680a5f471831b164
Allan Cutler <musicfan03@gmail.com>
2014-01-22 16:59:26 -0800

    New app constant for ngResource prefix escaping

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -1,5 +1,7 @@
 // Prefix url string for api calls
-app.constant("apiPrefix", "/api");
+app.constant("apiPrefix", "http://localhost:8000");
+app.constant("resourceApiPrefix", "http://localhost:8000\:8000");
+app.constant("cdnDomain", "http://staging.dirtyrottenrides.netdna-cdn.com");
 
 // Location for session information within sessionStorage
 // Related to authentication & session
