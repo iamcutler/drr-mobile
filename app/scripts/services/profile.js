38758ee99dbab7338a17c455859fb67a4dc541e2
Allan Cutler <musicfan03@gmail.com>
2014-01-22 17:13:23 -0800

    Modified profile data method in profile service to use $q and return a promise.

--- app/scripts/services/profile.js
+++ app/scripts/services/profile.js
@@ -1,16 +1,20 @@
 'use strict';
 
-app.factory('ProfileService', function ($http, $location, AuthService, apiPrefix) {
+app.factory('ProfileService', function ($http, $q, apiPrefix, AuthService) {
   return {
-    getProfileData: function (params, callback) {
-      if(typeof callback === "function") {
-        // if results are cached
-        $http.post(apiPrefix + "/profile/" + params.slug, {hash: AuthService.current_user.hash}).
-          success(function(data) {
-            return callback(data[0]);
-          }).
-          error(function(data) {});
-      }
+    getProfileData: function (params) {
+      var deferred = $q.defer();
+
+      $http.get(apiPrefix + "/profile/" + params.slug + "?user_hash=" + AuthService.current_user().hash).
+        success(function(response) {
+          console.log(response);
+          deferred.resolve();
+        }).
+        error(function() {
+          deferred.reject();
+        });
+
+      return deferred.promise;
     }
   };
 });
