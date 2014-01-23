ad6bf571c342cbf2b4eb4e40638417da02bbb074
Allan Cutler <musicfan03@gmail.com>
2014-01-22 17:07:32 -0800

    Refactored friend requests view and partial. Included a new directive to reject friend requests. Next functionality is a directive to accept a request and call the service successfully.

--- app/scripts/controllers/requests.js
+++ app/scripts/controllers/requests.js
@@ -1,8 +1,9 @@
 'use strict';
 
-app.controller('RequestsController', function ($scope, $state) {
+app.controller('RequestsController', function ($scope, $state, requests) {
   // Friend Requests
   if($state.current.name == "friend-requests") {
     $scope.title = "Friend Requests";
+    $scope.requests = requests;
   }
 });
---
+++ app/scripts/directives/user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+app.directive('declineFriendRequest', function(RequestService) {
+  return {
+    restrict: 'A',
+    link: function postLink(scope, element, attrs) {
+      element.click(function() {
+        var request = attrs.request;
+
+        if(request != undefined) {
+          // Disable button
+          element.attr('disabled','disabled');
+
+          // Call request decline method in requests service
+          RequestService.decline(request, function() {
+            element.parent().parent().parent().parent().fadeOut();
+            console.log(decline);
+          });
+        }
+      });
+    }
+  };
+});
