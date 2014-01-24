c667640399798c784966abb4bb2fcb18e84e3ebc
Allan Cutler <musicfan03@gmail.com>
2014-01-23 22:05:02 -0800

    Added new update action to use PUT requests and a new accept function to call the update connection service.

--- app/scripts/services/friend-requests.js
+++ app/scripts/services/friend-requests.js
@@ -1,8 +1,11 @@
 'use strict';
 
 app.service('RequestService', function FriendRequests($resource, $q, resourceApiPrefix, AuthService) {
-  var Requests = $resource(resourceApiPrefix + '/requests/:id', {
+  var Requests = $resource(resourceApiPrefix + '/connections/:id', {
+    id: '@id',
     user_hash: AuthService.current_user().hash
+  }, {
+    update: { method: 'PUT' }
   });
 
   return {
@@ -16,6 +19,15 @@ app.service('RequestService', function FriendRequests($resource, $q, resourceApi
       });
 
       return deferred.promise;
+    },
+    accept: function(id, callback) {
+      Requests.update({ id: id }, function(response) {
+        if(typeof callback === "function") {
+          callback(response);
+        }
+      }, function() {
+        console.log('Error accepting friend request');
+      });
     },
     decline: function(id, callback) {
       Requests.delete({ id: id }, function(response) {
