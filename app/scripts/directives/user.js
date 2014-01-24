7f83d0abf3a021752e502d1b95c3708a2a2da209
Allan Cutler <musicfan03@gmail.com>
2014-01-23 22:06:04 -0800

    Refactored friend request directive to keep code DRY. New conditional to read the action and call the correct service.

--- app/scripts/directives/user.js
+++ app/scripts/directives/user.js
@@ -1,20 +1,28 @@
 'use strict';
 
-app.directive('declineFriendRequest', function(RequestService) {
+app.directive('friendRequest', function(RequestService) {
   return {
     restrict: 'A',
     link: function postLink(scope, element, attrs) {
       element.click(function() {
-        var request = attrs.request;
+        var request = attrs.request,
+            action = attrs.action;
 
-        if(request != undefined) {
+        if(request != undefined && action != undefined) {
           // Disable button
           element.attr('disabled','disabled');
 
-          // Call request decline method in requests service
-          RequestService.decline(request, function() {
-            element.parent().parent().parent().parent().fadeOut();
-          });
+          if(action == 'accept') {
+            // Call request accept method in requests service
+            RequestService.accept(request, function() {
+              element.parent().parent().parent().parent().fadeOut();
+            });
+          } else if(action == 'decline') {
+            // Call request decline method in requests service
+            RequestService.decline(request, function() {
+              element.parent().parent().parent().parent().fadeOut();
+            });
+          }
         }
       });
     }
