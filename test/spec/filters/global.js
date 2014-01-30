4e1342e3d8368e106acb288b1a542ff2ed04d33d
Allan Cutler <musicfan03@gmail.com>
2014-01-29 18:10:38 -0800

    Testing specs for substr and MessageStructure filters.

---
+++ test/spec/filters/global.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('Filter: Global', function () {
+
+  // load the filter's module
+  beforeEach(module('DRRMobileApp'));
+
+  describe('Filter: substr', function() {
+    var substr;
+
+    beforeEach(inject(function ($filter) {
+      substr = $filter('substr');
+    }));
+
+    it('should return sub string', function() {
+      var text = 'Dirty Rotten Rides';
+      expect(substr(text, 0, 10)).toBe('Dirty Rott...');
+    });
+
+    it('should not return period tail on longer strings', function() {
+      var text = 'Dirty Rotten Rides';
+      expect(substr(text, 0, 100)).toBe(text);
+    });
+  });
+
+  describe('Filter: MessageStructure', function() {
+    var MessageStructure,
+        user;
+
+    beforeEach(inject(function($filter) {
+      MessageStructure = $filter('MessageStructure');
+      user = 100;
+    }));
+
+    it('should return secondary class when own user message', function() {
+      expect(MessageStructure(user, 100)).toBe('right');
+    });
+
+    it('should not return secondary class', function() {
+      expect(MessageStructure(user, 200)).toBeNull();
+    });
+  });
+});
