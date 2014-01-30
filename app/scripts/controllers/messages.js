9298fb2b0cb01afb6fdd42957e9a5e0ba7b72b6c
Allan Cutler <musicfan03@gmail.com>
2014-01-29 18:34:23 -0800

    Refactored view scope data to condition if null on thumbnail and a angular each loop to populate message thumbnails based on who the message is from.

--- app/scripts/controllers/messages.js
+++ app/scripts/controllers/messages.js
@@ -5,6 +5,12 @@ app.controller('MessagesController', function ($scope, $state, $location, $ancho
     $scope.title = "My Messages";
 
     $scope.messages = messages;
+
+    // Set message timestamps to js date local times
+    // Detect user messages and replace with user thumbnail
+    angular.forEach($scope.messages.messages, function(val, key) {
+      val.posted_on = moment.utc(val.posted_on).local();
+    });
   }
 
   if($state.current.name == "message-thread") {
@@ -21,6 +27,11 @@ app.controller('MessagesController', function ($scope, $state, $location, $ancho
 
     // Set message timestamps to js date local times
     angular.forEach($scope.thread.messages, function(val, key) {
+      if(val.from == AuthService.current_user().id) {
+        val.thumbnail = AuthService.current_user().thumbnail;
+      } else {
+        val.thumbnail = $scope.thread.user.thumbnail;
+      }
       val.posted_on = moment.utc(val.posted_on).local();
     });
 
@@ -41,6 +52,7 @@ app.controller('MessagesController', function ($scope, $state, $location, $ancho
             message: $scope.new_msg.message,
             from: $scope.new_msg.user,
             to: $scope.new_msg.recepient,
+            thumbnail: AuthService.current_user().thumbnail,
             posted_on: moment.utc(new Date()).local()
           });
           // Reset message to blank after successful save
