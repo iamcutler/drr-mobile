c518c88b2c12b0b6909fe06dbcfc75da1a47f421
Allan Cutler <musicfan03@gmail.com>
2014-01-29 18:13:31 -0800

    Introduced substr and MessageStructure app filters.

---
+++ app/scripts/filters/global.js
@@ -0,0 +1,21 @@
+'use strict';
+
+app.filter('substr', function () {
+  return function(input, start, max) {
+    if(input.length > max) {
+      return input.substr(start, max) + '...';
+    } else {
+      return input;
+    }
+  };
+});
+
+app.filter('MessageStructure', function() {
+  return function(input, user_id) {
+    if(input == user_id) {
+      return 'right';
+    }
+
+    return null;
+  }
+});
