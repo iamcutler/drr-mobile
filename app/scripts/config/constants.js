5275372b72df6442e6a073b2928651fcc96d3151
Allan Cutler <musicfan03@gmail.com>
2014-02-10 21:27:59 -0800

    New imgPlaceholder constant and added to the app scope.

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -1,7 +1,8 @@
 // Prefix url string for api calls
 app.constant("apiPrefix", "http://localhost:8000");
 app.constant("resourceApiPrefix", "http://localhost:8000\:8000");
-app.constant("cdnDomain", "http://staging.dirtyrottenrides.netdna-cdn.com");
+app.constant("cdnDomain", "http://development.dirtyrottenrides.netdna-cdn.com");
+app.constant("imgPlaceholder", "components/com_community/assets/user.png");
 
 // Location for session information within sessionStorage
 // Related to authentication & session
--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -1,8 +1,9 @@
 'use strict';
 
-app.controller("ApplicationController", function($scope, $state, $cookieStore, cdnDomain, AuthService) {
-  // Set CDN host in app scope
+app.controller("ApplicationController", function($scope, $state, $cookies, cdnDomain, imgPlaceholder, AuthService) {
+  // Set constants in app scope
   $scope.cdnDomain = cdnDomain;
+  $scope.imgPlaceholder = imgPlaceholder;
 
   // Check user access throughout the application and redirect if wrong privileges
   AuthService.check_user_access();
