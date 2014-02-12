f5b1efb4090f514da0bf3deafc44fb44c5015238
Allan Cutler <musicfan03@gmail.com>
2014-02-12 12:13:39 -0800

    Added find_event resource method to event service.

--- app/scripts/services/event.js
+++ app/scripts/services/event.js
@@ -1,6 +1,23 @@
 'use strict';
 
-angular.module('DRRMobileApp')
-  .service('Event', function Event() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+app.service('EventService', function EventService($resource, $q, resourceApiPrefix, AuthService) {
+  var resource = $resource(resourceApiPrefix + "/user/events/:id", {
+    id: '@id',
+    user_hash: AuthService.current_user().hash
   });
+
+  return {
+    find_event: function(id) {
+      var defer = $q.defer();
+
+      resource.get({ id: id }, function(response) {
+        defer.resolve(response);
+      }, function() {
+        console.error('Error fetching events service: find_event');
+        defer.reject();
+      });
+
+      return defer.promise;
+    }
+  };
+});
