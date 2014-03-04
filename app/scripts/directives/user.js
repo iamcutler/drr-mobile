4452b120bedbb736777c8635c184d0376a43f703
Allan Cutler <musicfan03@gmail.com>
2014-03-03 18:26:55 -0800

    Remove friend and send friend request directives.

--- app/scripts/directives/user.js
+++ app/scripts/directives/user.js
@@ -28,3 +28,58 @@ app.directive('friendRequest', function(RequestService) {
     }
   };
 });
+
+app.directive('sendFriendRequest', function(RequestService) {
+  return {
+    restrict: 'A',
+    scope: {
+      target: '@'
+    },
+    link: function(scope, elem, attrs) {
+      elem.bind('click', function() {
+        // Disable button on click to avoid duplicate requests
+        elem.prop('disabled', true);
+
+        var request = RequestService.send(scope.target);
+
+        request.then(function(response) {
+          if(response['result']) {
+            elem.text('Request Sent');
+          } else {
+            elem.text('Unable to send');
+          }
+        }, function(error) {
+          // Display error and make element available if error flags
+          elem.prop('disabled', true);
+          console.error(error);
+        });
+      });
+    }
+  };
+});
+
+app.directive('removeFriend', function(UserService) {
+  return {
+    restrict: 'A',
+    scope: {
+      target: '@'
+    },
+    link: function(scope, elem, attrs) {
+      elem.bind('click', function() {
+        // Make sure target is a number
+        if(angular.isDefined(scope.target)) {
+          elem.prop('disabled', true);
+
+          // Call remove friend function for User Service
+          UserService.remove_friend(scope.target).then(function(response) {
+            if(response.result) {
+              elem.text('Friend Removed');
+            } else {
+              elem.text('Removal Failed');
+            }
+          });
+        }
+      });
+    }
+  };
+});
