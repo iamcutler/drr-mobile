1655355a1850defa356435440aa252ec383784f9
Allan Cutler <musicfan03@gmail.com>
2014-03-04 10:55:40 -0800

    Added account settings view, user service and controller to save and update

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -1,8 +1,42 @@
 'use strict';
 
-app.controller('UserController', function ($scope, $state) {
+app.controller('UserController', function ($scope, $state, UserService, account) {
   // Account settings
   if($state.current.name = "account-settings") {
     $scope.title = "Account Settings";
+    $scope.settings = account;
+    $scope.perferences = {};
+    $scope.submitSuccess = false;
+
+    // Save account preferences
+    $scope.saveSettings = function(form) {
+      if(form.$valid) {
+        // Disable button to avoid double clicking
+        $('button').prop('disabled', true);
+
+        // Assign values to perferences scope
+        angular.forEach($scope.settings, function(val, key) {
+          $scope.perferences[key] = {
+            id: val.id,
+            value: val.value.value,
+            access: val.value.access
+          };
+        });
+
+        var update = UserService.update_account_settings($scope.perferences);
+
+        update.then(function(response) {
+          $scope.submitSuccess = true;
+          $('button').prop('disabled', false);
+          console.log(response);
+
+          if(response.status) {
+            $scope.statusMsg = 'Successfully saved account perferences.';
+          } else {
+            $scope.statusMsg = 'Error saving account perferences.';
+          }
+        });
+      }
+    };
   }
 });
--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -2,6 +2,39 @@
 
 app.factory('UserService', function UserService($http, $q, endPoint, AuthService) {
   return {
+    account_settings: function() {
+      var defer = $q.defer();
+
+      $http.get(endPoint.api + "/user/account/settings?user_hash=" + AuthService.current_user().hash)
+        .success(function(response) {
+          defer.resolve(response);
+        })
+        .error(function() {
+          defer.reject('Error fetching account settings');
+        });
+
+      return defer.promise;
+    },
+    update_account_settings: function(settings) {
+      var defer = $q.defer();
+
+      $http({
+        method: 'POST',
+        url: endPoint.api + "/user/account/settings",
+        data: {
+          settings: settings,
+          user_hash: AuthService.current_user().hash
+        }
+      })
+      .success(function(response) {
+        defer.resolve(response);
+      })
+      .error(function() {
+        defer.reject('Error updating account settings');
+      });
+
+      return defer.promise;
+    },
     remove_friend: function(id) {
       var defer = $q.defer();
 
