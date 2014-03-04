fae38a7c392fd80f0b864990f71d0580f17dceb9
Allan Cutler <musicfan03@gmail.com>
2014-03-03 23:24:56 -0800

    Added caching and new limit argument to user friends function.

--- app/scripts/services/profile.js
+++ app/scripts/services/profile.js
@@ -15,10 +15,12 @@ app.factory('ProfileService', function ($http, $q, endPoint, AuthService) {
 
       return deferred.promise;
     },
-    user_friends: function(params) {
+    user_friends: function(params, limit) {
       var deferred = $q.defer();
 
-      $http.get(apiPrefix + "/user/profile/friends/" + params.slug + "?user_hash=" + AuthService.current_user().hash).
+      $http.get(endPoint.api + "/user/profile/friends/" + params.slug + "/"+ limit +"?user_hash=" + AuthService.current_user().hash, {
+        cache: true
+      }).
         success(function(response) {
           deferred.resolve(response);
         }).
