0a9923fbea7945b725d8d613cb462e2677077762
Allan Cutler <musicfan03@gmail.com>
2014-03-11 16:18:20 -0700

    Added service and modified view to upload photo status

--- app/scripts/controllers/modal.js
+++ app/scripts/controllers/modal.js
@@ -1,10 +1,13 @@
 'use strict';
 
-app.controller('ModalController', function($scope, $modalInstance, ActivityService, MessageService, EventService, AuthService, user) {
+app.controller('ModalController', function($scope, $modalInstance, $upload, endPoint, ActivityService, MessageService, EventService, AuthService, user) {
   // Assign user to modal
   $scope.user = user;
   $scope.statusError = false;
   $scope.statusErrorMsg = '';
+  $scope.statusSubmitted = false;
+  $scope.photoProcessing = false;
+  $scope.uploadPercentage = 0;
 
   // Sending a new message to user from model
   $scope.newMessage = {
@@ -19,13 +22,13 @@ app.controller('ModalController', function($scope, $modalInstance, ActivityServi
       app: 'status'
     },
     photo: {
-      app: 'photo'
+      app: 'photo-status'
     },
     video: {
-      app: 'video'
+      app: 'video-status'
     },
     event: {
-      app: 'event'
+      app: 'event-status'
     }
   };
 
@@ -46,6 +49,57 @@ app.controller('ModalController', function($scope, $modalInstance, ActivityServi
         $scope.statusError = true;
         $scope.statusErrorMsg = response;
       });
+    }
+  };
+
+  // Photo Status
+  $scope.newPhotoStatus = function($files) {
+    var elem = $("div#photo-status form[name='newPhotoStatusForm']"),
+        progressBar = $('div#photo-status .progress');
+
+    // Hide form div and show loader
+    elem.hide();
+    progressBar.show();
+
+    //$files: an array of files selected, each file has name, size, and type.
+    for (var i = 0; i < $files.length; i++) {
+      var file = $files[i];
+      $scope.upload = $upload.upload({
+        url: endPoint.api + '/user/activity?user_hash=' + AuthService.current_user().hash,
+        method: 'POST',
+        // headers: {'headerKey': 'headerValue'},
+        // withCredentials: true,
+        data: $scope.new_status.photo,
+        file: file,
+        // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
+        /* set file formData name for 'Content-Desposition' header. Default: 'file' */
+        //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+        /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
+        //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
+        }).progress(function(evt) {
+          // Change upload percent scope to change progress bar
+          $scope.uploadPercentage = parseInt(100.0 * evt.loaded / evt.total);
+        }).success(function(data, status, headers, config) {
+          // file is uploaded successfully
+          console.log(data);
+          $modalInstance.dismiss();
+        }).error(function() {
+          elem.show();
+          progressBar.hide();
+        });
+    }
+  };
+
+  // Event Status
+  $scope.newEventStatus = function(form) {
+    if(form.$valid) {
+      $scope.statusError = false;
+      // Disable submit button to avoid multi calls
+      $("form[name='" + form.$name + "'] button[type='submit']").prop('disabled', true);
+
+
+    } else {
+      $scope.statusError = true;
     }
   };
 
--- app/scripts/services/activity.js
+++ app/scripts/services/activity.js
@@ -33,6 +33,44 @@ app.factory('ActivityService', function ActivityService($http, $resource, $q, en
 
       return defer.promise;
     },
+    media_upload: function(model) {
+      $http({
+        method: 'POST',
+        url: endPoint.api + "user/activity?user_hash=" + AuthService.current_user().hash,
+        //IMPORTANT!!! You might think this should be set to 'multipart/form-data'
+        // but this is not true because when we are sending up files the request
+        // needs to include a 'boundary' parameter which identifies the boundary
+        // name between parts in this multi-part request and setting the Content-type
+        // manually will not set this boundary parameter. For whatever reason,
+        // setting the Content-type to 'false' will force the request to automatically
+        // populate the headers properly including the boundary parameter.
+        headers: { 'Content-Type': false },
+        //This method will allow us to change how the data is sent up to the server
+        // for which we'll need to encapsulate the model data in 'FormData'
+        transformRequest: function (data) {
+          var formData = new FormData();
+          //need to convert our json object to a string version of json otherwise
+          // the browser will do a 'toString()' on the object which will result
+          // in the value '[Object object]' on the server.
+          formData.append("model", angular.toJson(data.model));
+          //now add all of the assigned files
+          for (var i = 0; i < data.files; i++) {
+            //add each file to the form data and iteratively name them
+            formData.append("file" + i, data.files[i]);
+          }
+          return formData;
+        },
+        //Create an object that contains the model and files which will be transformed
+        // in the above transformRequest method
+        data: { model: model, files: model.file }
+      })
+        .success(function (data, status, headers, config) {
+          console.log(data);
+        })
+        .error(function (data, status, headers, config) {
+          console.log(data);
+        });
+    },
     event_attendance: function(event_id) {
       var defer = $q.defer();
 
