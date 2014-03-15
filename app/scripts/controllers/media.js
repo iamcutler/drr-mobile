748279eaee0bb9a62554176c30fca06c88b4de00
Allan Cutler <musicfan03@gmail.com>
2014-03-14 17:09:51 -0700

    Added feed object and load property to be fired on the infinite scroll and popular media scope with new data.

--- app/scripts/controllers/media.js
+++ app/scripts/controllers/media.js
@@ -8,22 +8,44 @@ app.controller('MediaController', function ($scope, $state, media, ActivityServi
     });
   };
 
-  $scope.feed = {
-    load: function() {
-      console.log('loading');
-    }
-  };
-
   if($state.current.name == "media") {
     $scope.title = "Media";
     $scope.media = media;
     $scope.media_counter = 10;
     $scope.busy = false;
+    $scope.feedDisabled = false;
 
     // Format timestamps
     formatTimeStamp($scope.media);
 
-    console.log(media);
+    // media Feed object
+    $scope.feed = {
+      load: function() {
+        if(!$scope.feedDisabled) {
+          $scope.busy = true;
+          var media = ActivityService.media($scope.media_counter);
+
+          media.then(function(response) {
+            // Set $scope.busy back to falsy to hide loader and enable scrolling
+            $scope.busy == false;
+
+            if(response.length > 0) {
+              // Format new data to use moment js and convert to local timezone
+              formatTimeStamp(response);
+
+              // Loop thorugh and add new feed data to scope
+              angular.forEach(response, function(val, key) {
+                $scope.media.push(val);
+              });
+            } else {
+              $scope.feedDisabled = true;
+            }
+          }, function(error) {
+            console.error(error);
+          });
+        }
+      }
+    };
   }
 
   if($state.current.name == "activity") {
