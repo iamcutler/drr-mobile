26d992ac61d774754b9d9e6abcecc0f636385956
Allan Cutler <musicfan03@gmail.com>
2014-03-28 23:20:15 -0700

    Still tweaking activity spec

--- test/spec/directives/activity.js
+++ test/spec/directives/activity.js
@@ -12,9 +12,28 @@ describe('Directive: activity', function () {
     scope = $rootScope.$new();
   }));
 
-  xit('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<activity></activity>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the activity directive');
-  }));
+  describe('Event attendance directive', function() {
+    beforeEach(inject(function($compile) {
+      element = angular.element('<select class="form-control" target="1" event-attendance><option value="0">Not Attending</option><option value="1">Attending</option></select>');
+      element = $compile(element)(scope);
+      scope.$digest();
+    }));
+
+    xit('eventAttendance directive', inject(function() {
+      element.change();
+
+      //expect(element).toBe;
+    }));
+  });
+
+  describe('Remove wall directive', function() {
+    beforeEach(inject(function($compile) {
+      element = angular.element('<span model="activity.comments" target="1" index="0" remove-wall></span>');
+      element = $compile(element)(scope);
+    }));
+
+    it('', function() {
+
+    });
+  });
 });
