aff8b5fce9924c92ad1f617de44475e1db0d24d7
Allan Cutler <musicfan03@gmail.com>
2014-04-06 20:45:16 -0700

    Added infinite scrolling to find people controller and view. Refactored existing function to do initial call and firing on scroll. Continuing to tweak scrolling distance.

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -6,12 +6,14 @@ app.controller('SearchController', ['$scope', '$state', 'SearchService', functio
   {
     $scope.title = "Find People";
     $scope.loading = false;
+    $scope.load_counter = 0;
+    $scope.scroll_disabled = false;
     $scope.search = {
       // Params to pass to backend
       params: {
         q: '',
         offset: 0,
-        type: 'name',
+        type: 'name'
       },
       // Search results object
       results: []
@@ -20,16 +22,34 @@ app.controller('SearchController', ['$scope', '$state', 'SearchService', functio
     // Function to process search string
     $scope.search_users = function(data) {
       $scope.loading = true;
+      $scope.scroll_disabled = true;
+
+      // Conditional on scroll action
+      if(data.action === undefined) {
+        $scope.load_counter = 0;
+        $scope.search.results = [];
+      }
+
       // Call search service
-      var search = SearchService.find_people(data);
+      var search = SearchService.find_people(data),
+          offset = 20;
 
       search.then(function(response) {
         $scope.loading = false;
 
+        // add increments of 20 after successfull call
+        // Function used for init call and infinite scrolling
+        if(response.length > 0 || response.length < offset) {
+          // Disable scrolling if response data has results
+          $scope.scroll_disabled = false;
+          $scope.load_counter += offset;
+        }
+
         // Empty any previous results
-        if($scope.search.results.length > 0) {
+        if($scope.search.results.length > 0 && $scope.load_counter == 0) {
           $scope.search.results = [];
         }
+
         // Add response to results object scope
         angular.forEach(response, function(value, key) {
           $scope.search.results.push(value);
@@ -37,6 +57,19 @@ app.controller('SearchController', ['$scope', '$state', 'SearchService', functio
       }, function(error) {
         console.error(error);
       });
+    };
+
+    // Function fired on infinite scrolling
+    $scope.load_search_users = function() {
+      var search_params = {
+        q: $scope.search.params.q,
+        offset: $scope.load_counter,
+        type: 'name',
+        action: 'scroll'
+      };
+
+      // Call function to populate results
+      $scope.search_users(search_params);
     };
   }
 }]);
