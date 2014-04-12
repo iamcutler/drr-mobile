437d3182b9b34d1fa2d2e6fdb7163211a167ebec
Allan Cutler <musicfan03@gmail.com>
2014-04-12 03:01:32 -0700

    Remove modal and show loader when voting in image window and on success

--- app/scripts/directives/vote.js
+++ app/scripts/directives/vote.js
@@ -12,13 +12,20 @@ app.directive('castVote', ['$location', 'VotingService', function($location, Vot
     link: function postLink(scope, element, attrs) {
       element.click(function() {
         // Disable all buttons after click to avoid double clicking or voting
-        $('ul.voting-options li div.vote-action button').attr('disabled', 'disabled');
+        $('ul.voting-options li div.vote-action button').prop('disabled', true);
 
         // Show vote button loader
         element.children('button').addClass('loading');
+        element.children('button').prop('disabled', true);
 
         // Call voting service to save vote
         VotingService.cast_vote(attrs.poll, attrs.answer, function(response) {
+          element.children('button').removeClass('loading');
+          // Hide any open modal
+          $('div.modal').toggleClass('in','out');
+          $('div.modal-backdrop').toggleClass('in','out');
+          $('div.modal').delay(300).remove();
+          $('div.modal-backdrop').delay(300).remove();
           // Set date object locally based on last vote to track voting periods
           VotingService.set_vote_timestamp();
           // Redirect to results
