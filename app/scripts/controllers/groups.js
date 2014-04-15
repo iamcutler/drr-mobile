d643c4641423d414a189b3caceb2fae640376c78
Allan Cutler <musicfan03@gmail.com>
2014-04-15 01:14:45 -0700

    Made modifications to events for this release. Backlogged extra group features

--- app/scripts/controllers/groups.js
+++ app/scripts/controllers/groups.js
@@ -8,5 +8,19 @@ app.controller('GroupController', ['$scope', '$state', '$stateParams', '$locatio
     $scope.group = group;
     // Format group timestamp created to utc local
     $scope.group.created = moment.utc($scope.group.created).local();
+
+    // Format announcements timestamps
+    $scope.$watch(group.announcements, function() {
+      angular.forEach(group.announcements, function(value, key) {
+        group.announcements[key].date = moment.utc(group.announcements[key].date).local();
+      });
+    });
+
+    // Format discussion timestamps
+    $scope.$watch(group.discussions, function() {
+      angular.forEach(group.discussions, function(value, key) {
+        group.discussions[key].created = moment.utc(group.discussions[key].created).local();
+      });
+    });
   }
 }]);
