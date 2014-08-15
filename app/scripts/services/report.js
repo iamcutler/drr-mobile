09626368b53cc44ab2f5fb4b506361700367fcf2
Allan Cutler <musicfan03@gmail.com>
2014-08-15 13:19:37 -0700

    Added report service unit test for reporting bugs.

--- app/scripts/services/report.js
+++ app/scripts/services/report.js
@@ -4,13 +4,6 @@ app.factory('ReportService', ['$http', '$rootScope', 'endPoint', 'AuthService', 
   // AngularJS will instantiate a singleton by calling "new" on this function
   return {
     bug: function(data, callback) {
-      console.log({
-        user_hash: AuthService.current_user().hash,
-        category: data.category,
-        message: data.message,
-        bug_type: data.type
-      });
-
       $http.post(endPoint.api + '/report/bug', {
         user_hash: AuthService.current_user().hash,
         category: data.category,
--- test/spec/services/report.js
+++ test/spec/services/report.js
@@ -1,18 +1,58 @@
 'use strict';
 
-xdescribe('Service: Report', function () {
+describe('Service: Report', function () {
 
   // load the service's module
-  beforeEach(module('DrrmobileApp'));
+  beforeEach(module('DRRMobileApp'));
 
   // instantiate service
-  var Report;
-  beforeEach(inject(function (_Report_) {
-    Report = _Report_;
+  var ReportService,
+      httpBackend,
+      endPoint,
+      AuthService,
+      fakeReport;
+
+  beforeEach(inject(function($httpBackend, _endPoint_, _ReportService_, _AuthService_) {
+    ReportService = _ReportService_;
+    httpBackend = $httpBackend;
+    endPoint = _endPoint_;
+    AuthService = _AuthService_;
+
+    // Set current user
+    AuthService.set_user_session({
+      id: 1,
+      name: 'John Doe',
+      username: 'johndoe',
+      thumbnail: '',
+      slug: '',
+      hash: '6546516541651'
+    });
+
+    fakeReport = {
+      category: 'Profile',
+      message: 'Testing report',
+      bug_type: 'mobile-app'
+    };
+
+    httpBackend.when("GET", "/views/layouts/default.html").respond(200, {});
+    httpBackend.when("GET", "/views/news-feed/index.html").respond(200, {});
+    httpBackend.when('GET', endPoint.api + '/user/feed_activity/0?user_hash=' + AuthService.current_user().hash).respond(200, {});
+
+    httpBackend.flush();
   }));
 
-  it('should do something', function () {
-    expect(!!Report).toBe(true);
+  afterEach(function() {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
   });
 
+  it('report application bug', function () {
+    httpBackend.when('POST', endPoint.api + '/report/bug').respond(200, { status: true });
+
+    ReportService.bug(fakeReport, function(response) {
+      expect(response.status).toEqual(true);
+    });
+
+    httpBackend.flush();
+  });
 });
